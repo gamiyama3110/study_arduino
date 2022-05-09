09306afdadedcb7955322fd4d68afd1bdf7ca3ee
gamiyama3110 <gamiyamatatoo3198@gmail.com>
2022-05-09 23:33:52 +0900

    送信ボタンの無効時間追加、赤外線受信の変化検知を実装

--- ir_receive/ir_receive.ino
+++ ir_receive/ir_receive.ino
@@ -9,32 +9,45 @@ long current_time, indicator_timer;
 
 IRrecv receiver(receive_pin);
 decode_results results;
+unsigned long transmission;
 
 void setup() {
   Serial.begin(9600);
   receiver.enableIRIn();
   pinMode(indicator_pin, OUTPUT);
   pinMode(sender_button_pin, INPUT);
-  
 }
 
 void loop() {
   current_time = millis();
-
-  if (digitalRead(sender_button_pin) == HIGH) {
-    indicator_timer = current_time + 1000;
+  
+  if (digitalRead(sender_button_pin) == HIGH && isEnabledSenderButton()) {
+    indicator_timer = createTimeFromCurrentTime(1000); // インジケーターの点滅時間
+    pins_timer[sender_button_pin] = createTimeFromCurrentTime(200); // ボタンの無効時間
     Serial.println("pushed");
     
-  } else if (receiver.decode(&results))  {
-//    Serial.println(results.value, HEX);
-    Serial.println(results.value);
-    indicator_timer = current_time + 1000;
+  } else if (receiver.decode(&results) && isChangedDataForTransmission(results))  {
+    transmission = results.value;
+    Serial.println(transmission, HEX);
+//    Serial.println(transmission);
+    indicator_timer = createTimeFromCurrentTime(1000);
     receiver.resume();
     
   }
 
   flash_led(indicator_pin, indicator_timer);
-//  delay(10);
+}
+
+bool isChangedDataForTransmission(decode_results val) {
+  return transmission != val.value;
+}
+
+bool isEnabledSenderButton() {
+  return current_time > pins_timer[sender_button_pin];
+}
+
+long createTimeFromCurrentTime(long gap) {
+  return current_time + gap;
 }
 
 /*
@@ -46,11 +59,13 @@ void flash_led(int pin, long time) {
   bool terminated = time - current_time < 0;
   long gap = current_time - pins_timer[pin];
   bool lighted = digitalRead(pin)==HIGH;
-  
+
   if (!terminated &&  gap > 300) {
     digitalWrite(pin, lighted ?0:1);
     pins_timer[pin] = current_time;
-    
+
+    Serial.print("light:");
+    Serial.println(lighted);
   } else if (terminated && lighted) {
     digitalWrite(pin, LOW);
   }
