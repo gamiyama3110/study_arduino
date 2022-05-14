0749dad5219edee902c358a153a72434759e30a6
gamiyama3110 <gamiyamatatoo3198@gmail.com>
2022-05-14 16:58:09 +0900

    ir dump機能追加

--- ir_receive/ir_receive.ino
+++ ir_receive/ir_receive.ino
@@ -8,7 +8,6 @@ long pins_timer[13];
 long current_time, indicator_timer;
 
 IRrecv receiver(receive_pin);
-decode_results results;
 unsigned long transmission;
 
 void setup() {
@@ -16,26 +15,43 @@ void setup() {
   receiver.enableIRIn();
   pinMode(indicator_pin, OUTPUT);
   pinMode(sender_button_pin, INPUT);
+
+  digitalWrite(indicator_pin, HIGH);
+  delay(200);
+  digitalWrite(indicator_pin, LOW);
+  delay(200);
+  Serial.println("start");
 }
 
 void loop() {
   current_time = millis();
+  decode_results results;
   
   if (digitalRead(sender_button_pin) == HIGH && isEnabledSenderButton()) {
     indicator_timer = createTimeFromCurrentTime(1000); // インジケーターの点滅時間
     pins_timer[sender_button_pin] = createTimeFromCurrentTime(200); // ボタンの無効時間
     Serial.println("pushed");
     
-  } else if (receiver.decode(&results) && isChangedDataForTransmission(results))  {
+  } else if (receiver.decode(&results))  {
+    if (!isChangedDataForTransmission(results)) {
+      receiver.resume();
+      goto skip;
+    }
+    
     transmission = results.value;
-    Serial.println(transmission, HEX);
+//    Serial.println(transmission, HEX);
 //    Serial.println(transmission);
+    dump(&results);
     indicator_timer = createTimeFromCurrentTime(1000);
     receiver.resume();
+    Serial.println("saved");
     
   }
+  skip:
 
   flash_led(indicator_pin, indicator_timer);
+
+//  free(&results);
 }
 
 bool isChangedDataForTransmission(decode_results val) {
@@ -48,6 +64,63 @@ bool isEnabledSenderButton() {
 
 long createTimeFromCurrentTime(long gap) {
   return current_time + gap;
+}
+
+// Dumps out the decode_results structure.
+// Call this after IRrecv::decode()
+// void * to work around compiler issue
+//void dump(void *v) {
+//  decode_results *results = (decode_results *)v
+void dump(decode_results *results) {
+  int count = results->rawlen;
+  if (results->decode_type == UNKNOWN) {
+    Serial.print("Unknown encoding: ");
+  } 
+  else if (results->decode_type == NEC) {
+    Serial.print("Decoded NEC: ");
+  } 
+  else if (results->decode_type == SONY) {
+    Serial.print("Decoded SONY: ");
+  } 
+  else if (results->decode_type == RC5) {
+    Serial.print("Decoded RC5: ");
+  } 
+  else if (results->decode_type == RC6) {
+    Serial.print("Decoded RC6: ");
+  }
+  else if (results->decode_type == PANASONIC) {  
+    Serial.print("Decoded PANASONIC: ");
+//    Serial.print("Decoded PANASONIC - Address: ");
+//    Serial.print(results->panasonicAddress,HEX);
+//    Serial.print(" Value: ");
+  }
+  else if (results->decode_type == LG) {
+     Serial.print("Decoded LG: ");
+  }
+  else if (results->decode_type == JVC) {
+     Serial.print("Decoded JVC: ");
+  }
+  Serial.print(results->value, HEX);
+  Serial.print(" (");
+  Serial.print(results->bits, DEC);
+  Serial.println(" bits)");
+  Serial.print("Raw (");
+  Serial.print(count, DEC);
+  Serial.print("): ");
+
+  for (int i = 0; i < count; i++) {
+    if ((i % 2) == 1) {
+      Serial.print(results->rawbuf[i]*USECPERTICK, DEC);
+    } 
+    else {
+      Serial.print(-(int)results->rawbuf[i]*USECPERTICK, DEC);
+    }
+    Serial.print(" ");
+  }
+  Serial.println("\n end");
+
+//  free(results);
+//  free(count);
 }
 
 /*
@@ -64,9 +137,13 @@ void flash_led(int pin, long time) {
     digitalWrite(pin, lighted ?0:1);
     pins_timer[pin] = current_time;
 
-    Serial.print("light:");
-    Serial.println(lighted);
+//    Serial.print("light:");
+//    Serial.println(lighted);
   } else if (terminated && lighted) {
     digitalWrite(pin, LOW);
   }
+
+//  free(terminated);
+//  free(gap);
+//  free(lighted);
 }
